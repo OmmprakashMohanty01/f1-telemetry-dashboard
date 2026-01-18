e8e528c824a759ed83ac925356e68f829eabd330
OmmprakashMohanty01 <ommmohanty419@gmail.com>
2026-01-18 20:30:00 +0530

    more fixes for endpoint handling

---
+++ lap_delta_calculator.py
@@ -0,0 +1,16 @@
+import logging
+from typing import List
+
+logger = logging.getLogger(__name__)
+
+
+def compare_sector_times(sector_times_1, sector_times_2):
+    if len(sector_times_1) != len(sector_times_2):
+        raise ValueError("Sector times must have the same length")
+
+    performance_delta = []
+    for i in range(len(sector_times_1)):
+        delta = (sector_times_2[i] - sector_times_1[i]) / sector_times_1[i] * 100
+        performance_delta.append(delta)
+
+    return performance_delta
